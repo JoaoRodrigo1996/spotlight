0e1e6806a9eea84599834b9568237d1bbd6f8c69
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-01-17 15:49:31 -0300

    feat: setup nextjs and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/pages/**/*.tsx", "src/components/**/*.tsx"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
